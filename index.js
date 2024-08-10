2ee9fc4636c2c47542ff522935357dc8d8ef8476
srikarboga <boggavarapu.srikar@gmail.com>
2024-08-10 00:49:20 -0400

    Rectangles working on one client, TODO: add support for multiple clients

--- index.js
+++ index.js
@@ -1,54 +1,127 @@
 /** @type {HTMLCanvasElement} */
-// Get the canvas element
 const canvas = document.getElementById('Canvas');
 const ctx = canvas.getContext('2d');
-
-// Array to store rectangles
 let rectangles = [];
-
-// Rectangle size
 const rectSize = 20;
+let isDragging = false;
+let dragIndex = -1;
+let dragOffsetX = 0;
+let dragOffsetY = 0;
+let frameCount = 0;
+let lastTime = performance.now();
+let fps = 0;
 
-// Function to draw all rectangles
+class Rectangle {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.roundRect(this.x - rectSize / 2, this.y - rectSize / 2, rectSize, rectSize, rectSize / 5);
+        ctx.fill();
+    }
+
+    isPointInside(x, y) {
+        return (x >= this.x - rectSize / 2 && x <= this.x + rectSize / 2 &&
+            y >= this.y - rectSize / 2 && y <= this.y + rectSize / 2);
+    }
+}
+
 function drawRectangles() {
+    const currentTime = performance.now();
+    frameCount++;
+
+    if (frameCount % 100 === 0) {
+        fps = Math.round((frameCount * 1000) / (currentTime - lastTime));
+        frameCount = 0;
+        lastTime = currentTime;
+    }
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    rectangles.forEach(rect => {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(rect.x - rectSize / 2, rect.y - rectSize / 2, rectSize, rectSize);
-    });
+
+    // Draw rectangles
+    rectangles.forEach(rect => rect.draw());
+
+    // Draw FPS counter
+    ctx.fillStyle = 'white';
+    ctx.font = '16px Arial';
+    ctx.fillText(`FPS: ${fps}`, 10, 30);
+
+    // Request the next animation frame
+    window.requestAnimationFrame(drawRectangles);
 }
 
-// Function to add a rectangle
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+const randomHsl = () => `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
+
 function addRectangle(x, y) {
-    rectangles.push({ x, y });
-    drawRectangles();
+    rectangles.push(new Rectangle(x, y, randomHsl()));
+    dragIndex = rectangles.length - 1;
+    isDragging = true;
 }
 
-// Function to remove a rectangle
 function removeRectangle(x, y) {
-    rectangles = rectangles.filter(rect =>
-        !(x >= rect.x - rectSize / 2 && x <= rect.x + rectSize / 2 &&
-            y >= rect.y - rectSize / 2 && y <= rect.y + rectSize / 2)
-    );
-    drawRectangles();
+    rectangles = rectangles.filter(rect => !rect.isPointInside(x, y));
 }
 
-// Left click event listener
-canvas.addEventListener('click', function(e) {
+function getMousePos(e) {
     const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    addRectangle(x, y);
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+
+canvas.addEventListener('mousedown', function(e) {
+    const pos = getMousePos(e);
+    if (e.button === 0) { // Left click
+        for (let i = rectangles.length - 1; i >= 0; i--) {
+            if (rectangles[i].isPointInside(pos.x, pos.y)) {
+                isDragging = true;
+                dragIndex = i;
+                dragOffsetX = pos.x - rectangles[i].x;
+                dragOffsetY = pos.y - rectangles[i].y;
+                return;
+            }
+        }
+        addRectangle(pos.x, pos.y);
+    } else if (e.button === 2) { // Right click
+        removeRectangle(pos.x, pos.y);
+    }
 });
 
-// Right click event listener
+canvas.addEventListener('mousemove', function(e) {
+    if (isDragging) {
+        const pos = getMousePos(e);
+        rectangles[dragIndex].x = pos.x - dragOffsetX;
+        rectangles[dragIndex].y = pos.y - dragOffsetY;
+    }
+});
+
+canvas.addEventListener('mouseup', function() {
+    isDragging = false;
+    dragIndex = -1;
+});
+
 canvas.addEventListener('contextmenu', function(e) {
-    e.preventDefault(); // Prevent the default context menu
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    removeRectangle(x, y);
+    e.preventDefault();
 });
 
-// Initial draw
+canvas.style.backgroundColor = '#414141';
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.body.style.border = '0';
+document.body.style.overflow = 'hidden';
+
+resizeCanvas();
 drawRectangles();
+window.addEventListener('resize', resizeCanvas);
